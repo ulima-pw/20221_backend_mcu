ffc07daaa7ae6a7051085ab766e57bfdf85d3e27
ProfL3502 <ProfL3502@lab.aca>
2022-06-21 13:04:30 -0500

    Aumentado el filtro de peliculas por fase

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 import Express from "express";
+import data from "./resources/data.js";
+import cors from "cors"
 
 const app = Express()
 const PORT = 5000
+
+// Configuro para recibir cualquier peticion de cualquier
+// cliente
+app.use(cors())
+
+// Endpoint; GET /movies
+// http://localhost:5000/movies?fase=1
+app.get("/movies", (req, res) => {
+    const fase = req.query.fase
+
+    const listaPeliculas = data.data
+
+    if (fase != null) {
+        const resultado = []
+        listaPeliculas.forEach(pel => {
+            if (pel.phase == fase ){
+                resultado.push(pel)
+            }
+        });
+        // transformar data -> texto (string)
+        res.send(JSON.stringify({ data : resultado }))
+    }else {
+        res.send(JSON.stringify({ data : listaPeliculas }))
+    }
+
+    
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en puerto ${PORT}`)
